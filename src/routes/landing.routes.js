99936845e5a5c848f25b3c543b51819ad34b91c3
JoseRamirezV <jr.ramirez.varon@gmail.com>
2024-11-13 19:58:21 -0500

    ADD product links to navbar and product pages

--- src/routes/landing.routes.js
+++ src/routes/landing.routes.js
@@ -1,3 +1,5 @@
+const categories = require('../../categories.json');
+const products = require('../../products.json');
 const express = require('express');
 const specialOffers = require('../../specialOffers.json');
 
@@ -5,6 +7,35 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
    res.render('home', { title: 'Página de Inicio', specialOffers });
+});
+
+router.get('/categories', (req, res) => {
+   res.render('categories', { categories });
+});
+
+router.get('/consoles', (req, res) => {
+   const filteredProducts = products.filter(
+      (product) => product.category === 'console'
+   );
+   res.render('brochure', { category: 'Consolas', filteredProducts });
+});
+
+router.get('/laptops', (req, res) => {
+   const filteredProducts = products.filter(
+      (product) => product.category === 'laptop'
+   );
+   res.render('brochure', { category: 'Laptops', filteredProducts });
+});
+
+router.get('/peripherals', (req, res) => {
+   const filteredProducts = products.filter(
+      (product) => product.category === 'peripheral'
+   );
+   res.render('brochure', { category: 'Periféricos', filteredProducts });
+});
+
+router.get('/*', (req, res) => {
+   res.redirect('/');
 });
 
 module.exports = router;
